5e7979333e21d859a215c597db1a86d3b1321e7f
Oscar Rojas <oscaralejandrorojas@hotmail.com>
2025-03-25 15:19:19 -0500

    Informative content in registerSuccess page.

--- src/paginas/RegisterSuccess.js
+++ src/paginas/RegisterSuccess.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Form, ButtonToolbar, Button } from 'react-bootstrap';
 import Titulo from '../componentes/Titulo/titulo';
 import ContenedorPrincipal from '../componentes/ContenedorPrincipal/ContenedorPrincipal';
+import { useNavigate } from 'react-router-dom';
 
 export default function RegisterSuccess() {
-
+  const navigate = useNavigate();
   return(
     <ContenedorPrincipal>
         <Titulo 
           principal={"Se ha registrado exitosamente"}
-          secundario={"Por favor revise su correo electrónico."} />
+          secundario={"En caso de enviar correo electrónico, " +
+          "esta será la pantalla informativa."} />
+        <Container className='bg-light border rounded p-2 w-90'>
+          <Form>
+              <ButtonToolbar className="justify-content-end">
+              <Button 
+                  variant="secondary"
+                  onClick={() => navigate('/login')}>
+                  Login
+              </Button>
+              </ButtonToolbar>
+          </Form>
+        </Container>
     </ContenedorPrincipal>
   );
 }
