897c67a72a9c1b45cab34a0f362bf0c4a56da174
Oscar Rojas <oscaralejandrorojas@hotmail.com>
2025-03-26 16:10:16 -0500

    Switch from <section> to <div>

--- src/componentes/ContenedorPrincipal/ContenedorPrincipal.js
+++ src/componentes/ContenedorPrincipal/ContenedorPrincipal.js
@@ -2,10 +2,10 @@ import * as React from "react";
 export default function ContenedorPrincipal ({ children })
 {
     return (
-        <section className='register-page-wrapper page'>
+        <div className='register-page-wrapper page'>
           <div className="register-page">
             { children }
           </div>
-        </section>
+        </div>
     );
 }
