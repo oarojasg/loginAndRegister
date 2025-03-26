6762cfcfd4f70de2762d4a6d2f7a898d1f463770
Oscar Rojas <oscaralejandrorojas@hotmail.com>
2025-03-26 16:13:13 -0500

    Anchor tag to GitHub repository.

--- src/general/App.js
+++ src/general/App.js
@@ -10,25 +10,41 @@ import Register from '../paginas/Register';
 import Login from '../paginas/Login';
 import Confirm from '../paginas/Confirm';
 import RegisterSuccess from '../paginas/RegisterSuccess';
+import Config from '../configs/config.json'
 
 function App() {
   const { token, setToken } = useToken();
   if(!token) {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Login setToken={setToken} />} />
-          <Route path="/login" element={<Login setToken={setToken} />} />
-          <Route path="/register" element={<Register />} />
-          <Route path='/default' element={<App />} />
-          <Route path='/registerSuccess' element={<RegisterSuccess />} />
-          <Route path='/confirm' element={<Confirm />} />
-        </Routes>
-      </BrowserRouter>
+      <>
+        <div className="wrapper">
+          <a 
+            className='putOnTop'
+            href={Config.GitHub.url}
+            target='_blank'
+            rel='noopener noreferrer'>
+            Go to GitHub repository
+          </a>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' element={<Login setToken={setToken} />} />
+              <Route path="/login" element={<Login setToken={setToken} />} />
+              <Route path="/register" element={<Register />} />
+              <Route path='/default' element={<App />} />
+              <Route path='/registerSuccess' element={<RegisterSuccess />} />
+              <Route path='/confirm' element={<Confirm />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </>
+      
     );
   }
   return (
     <div className="wrapper">
+      <a href={Config.GitHub.url}>
+        Go to GitHub repository
+      </a>
       <h1>Application</h1>
       <BrowserRouter>
         <Routes> 
